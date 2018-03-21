451b566a3958a07f995a1d2b3bc6349311946af6
bounouamustapha <em_bounoua@esi.dz>
2018-03-21 21:50:02 +0100

    nik saoudi

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,9 @@ import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import Chat from './components/chat';
 import EmailUs from './components/EmailUs';
+import accept from './components/accept';
 import {Platform} from 'react-native';
+
 const RouterComponent = () => {
     let style =null;
    if (Platform.OS === 'android') {
@@ -12,7 +14,8 @@ const RouterComponent = () => {
   return (
           <Router  navigationBarStyle={{ backgroundColor: '#238AC5' }}  titleStyle={{color :'#fff'}} sceneStyle={style}>
             <Scene key="root" >
-                  <Scene tabBarStyle={styles.tabBarStyle} key="login" initial component={LoginForm} hideNavBar  />
+                  <Scene tabBarStyle={styles.tabBarStyle} key="login"  component={LoginForm} hideNavBar  />
+                  <Scene tabBarStyle={styles.tabBarStyle} key="accept" initial component={accept} hideNavBar  />
                   <Scene tabBarStyle={styles.tabBarStyle} key="emailUs" title="راسلنا" component={EmailUs} />
                   <Scene tabBarStyle={styles.tabBarStyle} key="chat"  hideNavBar component={Chat} />
             </Scene >
@@ -31,4 +34,3 @@ tabBarStyle: {
 }
 
 export default RouterComponent;
-  
